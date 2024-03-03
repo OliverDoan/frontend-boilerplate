30e48adbef400f7c6d6dd5bb4db598d9e511ece2
OliverDoan <doanvietthuan1005@gmail.com>
2024-03-03 23:25:52 +0700

    feat: setup antd

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,26 @@
+import { Button, Input, Select, Typography } from 'antd'
+
 const Home = () => {
-  return <div>Home</div>
+  const fruits = ['1', '2', '3', '4']
+  return (
+    <>
+      <div>Home</div>
+      <Button type='primary' block loading icon={<h1>hi</h1>} className='test' onClick={() => {}}>
+        Primary Button
+      </Button>
+      <Input placeholder='hi' maxLength={10} prefix={<span>hello</span>} allowClear></Input>
+      <Select placeholder='abc' allowClear>
+        {fruits.map((d, index) => {
+          return (
+            <Select.Option key={index} value={d}>
+              {d}
+            </Select.Option>
+          )
+        })}
+      </Select>
+      <Typography>a</Typography>
+    </>
+  )
 }
 
 export default Home
--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,12 +1,41 @@
+import { ConfigProvider } from 'antd'
 import { createBrowserRouter } from 'react-router-dom'
 import App from 'src/App'
 import Home from 'src/pages/home'
 import NotFound from 'src/pages/not-found'
+import type { ThemeConfig } from 'antd'
+// import { theme } from 'antd'
 
+// const { getDesignToken, useToken } = theme
+// const globalToken = getDesignToken(config)
+const config: ThemeConfig = {
+  components: {
+    Button: {
+      colorPrimary: 'yellow',
+      algorithm: true // Enable algorithm
+    },
+    Input: {
+      colorPrimary: 'green',
+      algorithm: true // Enable algorithm
+    }
+  },
+  token: {
+    // Seed Token
+    colorPrimary: 'red',
+    borderRadius: 2,
+
+    // Alias Token
+    colorBgContainer: '#f6ffed'
+  }
+}
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: (
+      <ConfigProvider theme={config}>
+        <App />
+      </ConfigProvider>
+    ),
     errorElement: <NotFound />,
     children: [
       {
