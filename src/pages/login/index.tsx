875b28791d4f84eb50b901dbf97132bf18e09cb0
OliverDoan <doanvietthuan1005@gmail.com>
2024-03-04 22:27:47 +0700

    feat: call api login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,30 +1,36 @@
 import { Button, Form, Input } from 'antd'
 import { type Rule } from 'antd/es/form'
+import authApi from 'src/apis/authApi'
 
 type LoginState = {
-  username?: string
-  password?: string
+  email: string
+  password: string
 }
 const formRules: Record<keyof LoginState, Rule[]> = {
-  username: [{ required: true, message: 'Please input your username!' }],
+  email: [{ required: true, message: 'Please input your email!' }],
   password: [{ required: true, message: 'Please input your password!' }]
 }
 const Login = () => {
   const [form] = Form.useForm<LoginState>()
-  const onFinish = (values: LoginState) => {
-    console.log('Success:', values)
+  const onFinish = async () => {
+    const values = await form.validateFields()
+    try {
+      const { data } = await authApi.login(values)
+      console.log('🚀 ~ handleLogin ~ data:', data)
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      console.log('🚀 ~ handleLogin ~ error:', error.response.data)
+    }
   }
 
   return (
     <Form form={form} onFinish={onFinish}>
-      <Form.Item label='Username' name='username' rules={formRules.username}>
+      <Form.Item label='Username' name='username' rules={formRules.email}>
         <Input />
       </Form.Item>
-
       <Form.Item label='Password' name='password' rules={formRules.password}>
         <Input.Password />
       </Form.Item>
-
       <Button type='primary' htmlType='submit'>
         Submit
       </Button>
