ddf40fda2a7d2aea89426b0105c5f9a8ea2bfdeb
Doan Viet Thuan <thuan.dv@tasco.com.vn>
2024-12-26 22:37:41 +0700

    feat: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
+import HomePage from 'src/pages/home'
+import LoginPage from 'src/pages/login'
+
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/login' element={<LoginPage />} />
+    </Routes>
   )
 }
 
