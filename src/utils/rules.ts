0bcef832246a7adff555ed0db806af0c1e9ed4aa
Doan Viet Thuan <thuan.dv@tasco.com.vn>
2025-01-10 22:04:34 +0700

    feat: update rule email

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -10,12 +10,7 @@ const handleConfirmPasswordYup = (refString: string) => {
 }
 
 export const schema = yup.object({
-  email: yup
-    .string()
-    .required('Email là bắt buộc')
-    .email('Email không đúng định dạng')
-    .min(5, 'Độ dài từ 5 - 10 ký tự')
-    .max(10, 'Độ dài từ 5 - 10 ký tự'),
+  email: yup.string().required('Email là bắt buộc').email('Email không đúng định dạng'),
 
   password: yup
     .string()
