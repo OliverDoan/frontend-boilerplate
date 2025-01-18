87a369cc9e60c62c7bc29efbbde7bcda3b538353
Doan Viet Thuan <thuan.dv@tasco.com.vn>
2025-01-18 23:11:02 +0700

    feat: save localStorage

--- src/redux/reducer/user.reducer.ts
+++ src/redux/reducer/user.reducer.ts
@@ -1,30 +1,39 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axiosClient from 'src/api/axiosClient'
 import { AuthRequest, AuthResponse } from 'src/api/types/auth.type'
-import userApi from 'src/api/userApi'
 import StorageKeys from 'src/constants/storage-keys'
 
+const URL_REGISTER = 'api/v1/users'
+const URL_LOGIN = 'api/v1/auth/login'
+
 interface UserState {
-  user: AuthResponse
+  token: string
 }
 
 const initialState: UserState = {
-  user: localStorage.getItem(StorageKeys.USER) ? JSON.parse(localStorage.getItem(StorageKeys.USER) || '{}') : {}
+  token: localStorage.getItem(StorageKeys.TOKEN) || ''
 }
 
-export const register = createAsyncThunk('user/register', async (payload: AuthRequest) => {
-  const response = await userApi.register(payload)
-  return response
+export const register = createAsyncThunk('user/register', async (body: AuthRequest) => {
+  const response = await axiosClient.post<AuthResponse>(URL_REGISTER, body)
+  return response.data
 })
 
-export const login = createAsyncThunk('user/login', async (payload: { email: string; password: string }) => {
-  const response = await userApi.login(payload)
-  return response
+export const login = createAsyncThunk('user/login', async (body: { email: string; password: string }) => {
+  const response = await axiosClient.post<{ access_token: string; refresh_token: string }>(URL_LOGIN, body)
+  localStorage.setItem(StorageKeys.TOKEN, response.data.access_token)
+  return response.data
 })
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {}
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.token = action.payload.access_token
+    })
+  }
 })
 
 const userReducer = userSlice.reducer
